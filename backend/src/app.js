326d236c143dc3a6349ef2599f290198ee94c99c
WillQi <williamqipizza@gmail.com>
2023-01-07 16:08:14 -0700

    backend to handle login/register

--- backend/src/app.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { PORT } = require("./config");
-
-const app = express();
-
-app.listen(PORT, () => console.log(`Backend server is up and running on port ${PORT}`));
--- backend/src/config.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  PORT: parseInt(process.env.PORT) || 8000
-};
