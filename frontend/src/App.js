230ec5e1e0d48f5d7a4572a3dcf54864f46ca42d
brian <brian.qi138@gmail.com>
2023-01-07 16:28:37 -0700

    starting frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
   return (
@@ -7,19 +9,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the main page :0
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <form>
+          <label>
+            Name:
+            <input type="text" name="name"></input>
+            <input type="submit" value="Submit"></input>
+          </label>
+        </form>
       </header>
     </div>
   );
 }
+
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from "./App";
+import Register from "./Register";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from 'react-router';
 
@@ -10,7 +11,11 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
-  }
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
